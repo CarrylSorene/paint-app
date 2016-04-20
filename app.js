1afe3e254d1f4c5c2ab6a606745b1c22b06556db
Carryl Sorene <carrylsorene@gmail.com>
2016-04-20 09:12:20 +0100

    Load image file

--- app.js
+++ app.js
@@ -145,3 +145,31 @@ link.addEventListener("mouseover", update);
 link.addEventListener("focus", update);
 return link;
 };
+
+//load images from local files and urls
+function loadImageURL(cx, url) {
+  var image = document.createElement("img");
+  image.addEventListener("load", function() {
+    var color = cx.fillStyle, size = cx.lineWidth;
+    cx.canvas.width = image.width;
+    cx.canvas.height = image.height;
+    cx.drawImage(image, 0, 0);
+    cx.fillStyle = color;
+    cx.strokeStyle = color;
+    cx.lineWidth = size;
+  });
+  image.src = url;
+}
+
+controls.openFile = function(cx) {
+  var input = elt("input", {type: "file"});
+  input.addEventListener("change", function() {
+    if (input.files.length == 0) return;
+    var reader = new FileReader();
+    reader.addEventListener("load", function() {
+      loadImageURL(cx, reader.result);
+    });
+    reader.readAsDataURL(input.files[0]);
+  });
+  return elt("div", null, "Open file: ", input);
+};
